c9e719038a773d0427830b7d4931d9ca593a36da
Nithuna1 <nithunans@gmail.com>
2026-03-24 19:36:45 +0530

    loader

---
+++ app/admin/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center h-[70vh]">
+      <div className="w-10 h-10 border-4 border-blue-600 border-t-transparent rounded-full animate-spin"></div>
+    </div>
+  );
+}
